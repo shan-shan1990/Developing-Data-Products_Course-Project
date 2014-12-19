3a0e61f5fe43fc4d0ecb7d084895ae1b3fb7dc9b
shan-shan1990 <shaniquecameron@yahoo.com>
2014-12-19 16:50:33 -0500

    Update Server.R

--- Server.R
+++ Server.R
@@ -1,39 +1,54 @@
-#install.packages("shiny")
+  
 library(shiny)
+library(datasets)
 
-#the computer guesses an integer between
-number <- floor(runif(1,1,101))
-#print(number)
-numberGuessed <- function(guess, number) {
-        returnValue <- "Nothing entered yet."
-        if (guess > 100) {
-                #print(guess)
-                #print(class(guess))
-                #print(class(100))
-                returnValue <- 'Above 100.\nPlease make a selection between 1 and 100.'
-        }
-        else if (guess < 1) {
-                #print(guess)
-                returnValue <- 'Below 1.\nPlease make a selection between 1 and 100.'
-        }
-        else if (guess > number) {
-                returnValue <- 'Higher than the number.'
-        }
-        else if (guess < number) {
-                returnValue <- 'Lower than the number.'
-        }
-        else if (guess == number) {
-                returnValue <- 'Correct!'
-        }
-        returnValue
-}
-shinyServer( 
-        function(input, output) {
-                output$inputValue <- renderPrint({as.numeric(input$guess)})
-                #output$outputValue <- renderText({numberGuessed(input$guess, number)})
-                output$outputValue <- renderText({
-                        if (input$goButton == 0) "You have not guessed a number yet"
-                        else if (input$goButton >= 1) numberGuessed(as.numeric(input$guess), number)
-                })
-        }
-)
+# Define server logic required to summarize and view the selected
+# dataset
+shinyServer(function(input, output) {
+  
+  # By declaring datasetInput as a reactive expression we ensure 
+  # that:
+  #
+  #  1) It is only called when the inputs it depends on changes
+  #  2) The computation and result are shared by all the callers 
+  #    (it only executes a single time)
+  #
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars" = cars)
+  })
+  
+  # The output$caption is computed based on a reactive expression
+  # that returns input$caption. When the user changes the
+  # "caption" field:
+  #
+  #  1) This function is automatically called to recompute the 
+  #     output 
+  #  2) The new caption is pushed back to the browser for 
+  #     re-display
+  # 
+  # Note that because the data-oriented reactive expressions
+  # below don't depend on input$caption, those expressions are
+  # NOT called when input$caption changes.
+  output$caption <- renderText({
+    input$caption
+  })
+  
+  # The output$summary depends on the datasetInput reactive
+  # expression, so will be re-executed whenever datasetInput is
+  # invalidated
+  # (i.e. whenever the input$dataset changes)
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  
+  # The output$view depends on both the databaseInput reactive
+  # expression and input$obs, so will be re-executed whenever
+  # input$dataset or input$obs is changed. 
+  output$view <- renderTable({
+    head(datasetInput(), n = input$obs)
+  })
+})
